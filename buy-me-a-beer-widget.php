08b358ae6639dbc7d7bebda1db070b80e658f415
Daan van den Bergh <daan.van.den.bergh@tig.nl>
2019-03-15 16:26:17 +0100

    Move options form to separate file.

--- buy-me-a-beer-widget.php
+++ buy-me-a-beer-widget.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: Buy Me A Beer Widget
- * Plugin URI:
- * Description:
- * Version: 1.0
+ * Description: Add a PayPay donation form widget to your WordPress blog.
+ * Version: 1.0.1
  * Author: Daan van den Bergh
  * Author URI: https://daan.dev
  * License: GPL2v2 or later
  */
-define('BUY_ME_A_BEER_STATIC_VERSION', '1.0.0');
+
+define('BUY_ME_A_BEER_STATIC_VERSION', '1.0.1');
 
 /**
  * Register and Load Widget
@@ -109,110 +109,7 @@ class BuyMeABeer_Widget extends WP_Widget
      */
     public function form($instance)
     {
-        $hostedButtonId = (isset($instance['hosted_button_id'])) ? $instance['hosted_button_id'] : '1XXX234XXX4X5X';
-        ?>
-        <p>
-            <label for="<?= $this->get_field_id('hosted_button_id'); ?>">
-                <?php _e('Hosted Button ID:'); ?>
-            </label>
-            <input class="widefat"
-                   id="<?= $this->get_field_id('hosted_button_id'); ?>"
-                   name="<?= $this->get_field_name('hosted_button_id'); ?>"
-                   type="text" value="<?= esc_attr($hostedButtonId); ?>"/>
-        </p>
-        <?php
-        $title = (isset($instance['title'])) ? $instance['title'] : __('Buy Me A Beer!', 'bmab_widget');
-        ?>
-        <p>
-            <label for="<?= $this->get_field_id('title'); ?>">
-                <?php _e('Title:'); ?>
-            </label>
-            <input class="widefat"
-                   id="<?= $this->get_field_id('title'); ?>"
-                   name="<?= $this->get_field_name('title'); ?>"
-                   type="text" value="<?= esc_attr($title); ?>"/>
-        </p>
-        <?php
-        $description = (isset($instance['description'])) ? $instance['description'] : __('Please consider donating.', 'bmab_widget');
-        ?>
-        <p>
-            <label for="<?= $this->get_field_id('description'); ?>">
-                <?php _e('Description:'); ?>
-            </label>
-            <textarea class="widefat"
-                      id="<?= $this->get_field_id('description'); ?>"
-                      name="<?= $this->get_field_name('description'); ?>"><?= esc_attr($description); ?></textarea>
-        </p>
-        <?php
-        $amountTitle = (isset($instance['amount_title'])) ? $instance['amount_title'] : __('Choose Amount:', 'bmab_widget');
-        ?>
-        <p>
-            <label for="<?= $this->get_field_id('amount_title'); ?>">
-                <?php _e('Amount Title:'); ?>
-            </label>
-            <input class="widefat"
-                   id="<?= $this->get_field_id('amount_title'); ?>"
-                   name="<?= $this->get_field_name('amount_title'); ?>"
-                   type="text" value="<?= esc_attr($amountTitle); ?>"/>
-        </p>
-        <?php
-        $amountComment = (isset($instance['amount_comment'])) ? $instance['amount_comment'] : __('How much do you want to donate?', 'bmab_widget');
-        ?>
-        <p>
-            <label for="<?= $this->get_field_id('amount_comment'); ?>">
-                <?php _e('Amount Comment:'); ?>
-            </label>
-            <textarea class="widefat"
-                      id="<?= $this->get_field_id('amount_comment'); ?>"
-                      name="<?= $this->get_field_name('amount_comment'); ?>"><?= esc_attr($amountComment); ?></textarea>
-        </p>
-        <?php
-        $pmTitle = (isset($instance['pm_title'])) ? $instance['pm_title'] : __('Personal Message:', 'bmab_widget');
-        ?>
-        <p>
-            <label for="<?= $this->get_field_id('pm_title'); ?>">
-                <?php _e('Personal Message Title:'); ?>
-            </label>
-            <input class="widefat"
-                   id="<?= $this->get_field_id('pm_title'); ?>"
-                   name="<?= $this->get_field_name('pm_title'); ?>"
-                   type="text" value="<?= esc_attr($pmTitle); ?>"/>
-        </p>
-        <?php
-        $pmComment = (isset($instance['pm_comment'])) ? $instance['pm_comment'] : __('Leave a message!', 'bmab_widget');
-        ?>
-        <p>
-            <label for="<?= $this->get_field_id('pm_comment'); ?>">
-                <?php _e('Personal Message Comment:'); ?>
-            </label>
-            <textarea class="widefat"
-                      id="<?= $this->get_field_id('pm_comment'); ?>"
-                      name="<?= $this->get_field_name('pm_comment'); ?>"><?= esc_attr($pmComment); ?></textarea>
-        </p>
-        <?php
-        $buttonValue = (isset($instance['button_value'])) ? $instance['button_value'] : __('Donate', 'bmab_widget');
-        ?>
-        <p>
-            <label for="<?= $this->get_field_id('button_value'); ?>">
-                <?php _e('Donate Button Value:'); ?>
-            </label>
-            <input class="widefat"
-                   id="<?= $this->get_field_id('button_value'); ?>"
-                   name="<?= $this->get_field_name('button_value'); ?>"
-                   type="text" value="<?= esc_attr($buttonValue); ?>"/>
-        </p>
-        <?php
-        $afterForm = (isset($instance['after_form'])) ? $instance['after_form'] : __('Thank you for donating!', 'bmab_widget');
-        ?>
-        <p>
-            <label for="<?= $this->get_field_id('after_form'); ?>">
-                <?php _e('After form message:'); ?>
-            </label>
-            <textarea class="widefat"
-                      id="<?= $this->get_field_id('after_form'); ?>"
-                      name="<?= $this->get_field_name('after_form'); ?>"><?= esc_attr($afterForm); ?></textarea>
-        </p>
-        <?php
+        include(plugin_dir_path(__FILE__) . 'includes/options.phtml');
     }
     
     /**
